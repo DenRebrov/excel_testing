55af3e7d1efd0ba014974910205a73e9cbf8c796
Dmitriy Alifanov <cross@vgg.ru>
2024-12-24 13:44:34 +0300

    Add type2

--- app/controllers/excels_controller.rb
+++ app/controllers/excels_controller.rb
@@ -8,7 +8,7 @@ class ExcelsController < ActionController::Base
       p.workbook.add_worksheet(:name => "Work Hours") do |sheet|
 
         # Это делает формат со стороны гема
-        date_style = p.workbook.styles.add_style(types: [:date], format_code: 'dd-mm-yyyy hh:mm:ss', alignment: { horizontal: :center })
+        date_style = p.workbook.styles.add_style(types: [:date], format_code: 'dd.mm.yy hh:mm', alignment: { horizontal: :center })
 
         # Если хочешь подключить формат со стороны гема, раскомментируй style: date_style
         #
@@ -25,7 +25,7 @@ class ExcelsController < ActionController::Base
 
   def define_date
     # Поиграй с этой строчкой
-    "23.12.2024 12:15:30".to_datetime.strftime("%Y-%m-%d %H:%M:%S")
+    "23.12.2024 12:15:30".to_datetime.strftime("%d.%m.%y %H:%M")
 
     # Эти методы я нарыл и их пока изучаю, может быть с ними все сработает
   	# serial_date = Axlsx::DateTimeConverter.date_to_serial(DateTime.current)
